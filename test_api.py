6c964310a659b22ac17ce4d9322dfbab7987d88b
zuhaalfaraj <zuha.alfaraj@gmail.com>
2023-10-31 15:31:28 -0500

    update api test cases

--- test_api.py
+++ test_api.py
@@ -7,7 +7,7 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
 
-def test_predict_with_valid_data():
+def test_predict_with_cls_1():
     response = client.post(
         "/predict/",
         json={'age': 24,
@@ -26,18 +26,26 @@ def test_predict_with_valid_data():
     'native-country': 'united-states'},
     )
     assert response.status_code == 200
+    assert response.json() == {"prediction": 1}
 
 
-def test_predict_with_invalid_data():
+def test_predict_with_cls_0():
     response = client.post(
         "/predict/",
-        json={
-            'age': "thirty",
-            'relationship': 'not-in-family',
-            'race_val': 'white',
-            'sex_val': 'male',
-            'capital': 0,
-            'capital': 0,
-        },
+        json={'age': 43,
+         'workclass': 'private',
+         'fnlgt': 70055,
+         'education': 'some-college',
+         'education-num': 10,
+         'marital-status': 'married-civ-spouse',
+         'occupation': 'adm-clerical',
+         'relationship': 'husband',
+         'race': 'white',
+         'sex': 'male',
+         'capital-gain': 0,
+         'capital-loss': 0,
+         'hours-per-week': 40,
+         'native-country': 'united-states'},
     )
-    assert response.status_code != 200
+    assert response.status_code == 200
+    assert response.json() == {"prediction": 0}
